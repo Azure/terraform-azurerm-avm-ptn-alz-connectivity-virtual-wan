53d274353d7cc02c5939e012f5257bd33a7cad87
Jared Holgate <jaredholgate@microsoft.com>
2025-03-10 17:18:09 +0000

    feat: first iteration

---
+++ examples/full-multi-region/variables.tf
@@ -0,0 +1,42 @@
+variable "custom_replacements" {
+  type = object({
+    names                      = optional(map(string), {})
+    resource_group_identifiers = optional(map(string), {})
+    resource_identifiers       = optional(map(string), {})
+  })
+  default = {
+    names                      = {}
+    resource_group_identifiers = {}
+    resource_identifiers       = {}
+  }
+  description = "Custom replacements"
+}
+
+
+variable "management_resource_settings" {
+  type        = any
+  default     = {}
+  description = <<DESCRIPTION
+The settings for the management resources. Details of the settings can be found in the module documentation at https://registry.terraform.io/modules/Azure/avm-ptn-alz-management
+DESCRIPTION
+}
+
+variable "management_group_settings" {
+  type        = any
+  default     = {}
+  description = <<DESCRIPTION
+The settings for the management groups. Details of the settings can be found in the module documentation at https://registry.terraform.io/modules/Azure/avm-ptn-alz
+DESCRIPTION
+}
+
+variable "connectivity_type" {
+  type        = string
+  description = "The type of network connectivity technology to use for the private DNS zones"
+  default     = "hub_and_spoke_vnet"
+}
+
+variable "enable_telemetry" {
+  type        = bool
+  default     = false
+  description = "Flag to enable/disable telemetry"
+}
