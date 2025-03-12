8a286518be083ac4df5d235a37ee9ee1dedde69a
Jared Holgate <jaredholgate@microsoft.com>
2025-03-12 10:49:14 +0000

    change tfvars file name

--- examples/full-multi-region/pre.sh
+++ examples/full-multi-region/pre.sh
@@ -1,1 +1,3 @@
-curl -o terraform.tfvars https://raw.githubusercontent.com/Azure/alz-terraform-accelerator/refs/heads/main/templates/platform_landing_zone/examples/full-multi-region/virtual-wan.tfvars
+echo "Downloading and creating the test.auto.tfvars file from https://raw.githubusercontent.com/Azure/alz-terraform-accelerator/refs/heads/main/templates/platform_landing_zone/examples/full-multi-region/virtual-wan.tfvars..."
+curl -o test.auto.tfvars https://raw.githubusercontent.com/Azure/alz-terraform-accelerator/refs/heads/main/templates/platform_landing_zone/examples/full-multi-region/virtual-wan.tfvars
+echo "File downloaded successfully."
