52938c1c0c2b3891aed9c1896f25c191713e2fec
Yingjie Chen (BEYONDSOFT CONSULTING INC) <yingjiechen2012@gmail.com>
2026-09-17 16:25:45 +0530

    fix(deps): upgrade VNet module to v0.22.2 for sidecar import protection

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ moved {
 
 module "virtual_network_side_car" {
   source   = "Azure/avm-res-network-virtualnetwork/azurerm"
-  version  = "0.20.0"
+  version  = "0.22.2"
   for_each = local.sidecar_virtual_networks
 
   location             = each.value.location
